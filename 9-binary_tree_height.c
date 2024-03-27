187c9805d4ceaf0deedb6c2e85862791eec0ea9c
Mwan3kii <agathawanja303@gmail.com>
2024-03-27 20:39:39 +0300

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t le = 0, ri = 0;
 		le = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		re = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		ri = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((le > ri) ? le : ri);
 	}
 	return (0);
