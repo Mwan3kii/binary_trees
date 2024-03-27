9956f5140bd00ea10cb0b28cd50f0d33add59e24
Mwan3kii <agathawanja303@gmail.com>
2024-03-27 21:00:05 +0300

    is full

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t le = 0, ri = 0;
+
 		le = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		ri = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((le > ri) ? le : ri);
